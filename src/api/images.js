a7a3c643ec53b76415e17652b478ff3da9e1b43d
Saad <saad_anwer@live.com>
2020-11-04 01:27:34 +0500

    env init

---
+++ src/api/images.js
@@ -0,0 +1,10 @@
+const ASSET_PATH = '../assets/images/';
+
+const images = {
+  //   logo: require(`${ASSET_PATH}logo.png`),
+  //   rightArrow: require(`${ASSET_PATH}right-arrow.png`),
+  //   visa: require(`${ASSET_PATH}visa.png`),
+  //   leftArrow: require(`${ASSET_PATH}left-arrow.png`),
+};
+
+export default images;
